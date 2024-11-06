5a24575c44f62e003dd3151513fd847cad6a638c
Lenin Reyes <ReyesPerez.Lenin@gmail.com>
2024-11-06 18:14:01 +0000

    Project 2: 9 Test

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +17,49 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+ @RestController
 public class SocialMediaController {
+
+    AccountService accountService;
+
+    @Autowired
+    public SocialMediaController(AccountService accountService){
+        this.accountService = accountService;
+    }
+    
+    @PostMapping("/register")
+    public ResponseEntity<Account> register (@RequestBody Account account){
+        Account accountCont;
+        accountCont = accountService.registerNewUser(account);
+        if(accountCont==null)
+        {
+            return ResponseEntity.status(400).header("Content-Type", "application/json").body(account);
+        }
+        else if(accountCont.getAccountId()==0){
+            return ResponseEntity.status(409).header("Content-Type", "application/json").body(account);
+
+        }
+        else{
+            return ResponseEntity.status(200).header("Content-Type", "application/json").body(accountCont);
+
+        }
+
+    } 
+
+    @PostMapping("/login")
+    public ResponseEntity<Account> login (@RequestBody Account account){
+        Account accountCont;
+        accountCont = accountService.login(account);
+        if(accountCont.getAccountId()==0){
+            return ResponseEntity.status(401).header("Content-Type", "application/json").body(account);
+
+        }
+        else{
+            return ResponseEntity.status(200).header("Content-Type", "application/json").body(accountCont);
+
+        }
+    }
+
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,61 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerNewUser(Account account) //throws DuplicateRowException
+    {
+        Optional<Account> accountOpt = accountRepository.findByUsernameAndPassword(account.getUsername(),account.getPassword());
+ 
+        if (accountOpt.isPresent()) {
+            //throw new DuplicateRowException("Duplicate row found");
+            return account;//accountOpt.get();
+        } else {
+                account = accountRepository.save(account);
+                return account;          
+        }
+ 
+    }
+
+    public Account login(Account account){
+        Optional<Account> accountOpt = accountRepository.findByUsernameAndPassword(account.getUsername(),account.getPassword());
+        System.out.println("AccountOpt:"+accountOpt.isPresent());
+        if (accountOpt.isPresent()) {
+            System.out.println("esta presente");
+            return accountOpt.get();
+        }
+        else{
+            
+            System.out.println("no esta presente");
+            return account;
+        }
+    }
+
+
+
+    /*
+    public class DuplicateRowException extends RuntimeException {
+
+        public DuplicateRowException(String message) {
+            super(message);
+        }
+    }*/
+
+
+
 }
