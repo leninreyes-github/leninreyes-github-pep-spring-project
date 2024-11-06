f4dadd8dd85d36f08e94332b9bd333121b5f266d
Lenin Reyes <ReyesPerez.Lenin@gmail.com>
2024-11-06 20:10:37 +0000

    Project 2:13 2

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Message;
 import com.example.repository.MessageRepository;
 
 @Service
@@ -15,6 +16,26 @@ public class MessageService {
         this.messageRepository = messageRepository;
     }
 
-    
+    public Message createNewMessage(Message message) //throws DuplicateRowException
+    {
+       return message;
+        /* Optional<Account> accountOpt = accountRepository.findByUsernameAndPassword(account.getUsername(),account.getPassword());
+        
+        
+        if (accountOpt.isPresent()) {
+            //throw new DuplicateRowException("Duplicate row found");
+            account.setAccountId(0); //future review
+            return account;//accountOpt.get();
+        } else {
+            if(account.getUsername().length()>0&&account.getPassword().length()>=4){
+                account = accountRepository.save(account);
+                return account;   
+            }
+            else{
+                return null;   //future review
+            }       
+        }
+        */
+    }    
 
 }
